872ee581935005962eaeb45e9e2bb323e4563189
OrghoN <anoronyo@gmail.com>
2022-11-28 12:39:15 -0600

    merges all output files stored in data directory

---
+++ mergeOutput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat data/event_eff_err-*.csv > event_eff_err.csv
+
+cat data/min_max_cut1*.csv > min_max_cut1.csv
+cat data/min_max_cut2*.csv > min_max_cut2.csv
+cat data/min_max_cut3*.csv > min_max_cut3.csv
+
+cat data/pion_pT_eta*.csv > pion_pT_eta.csv
+cat data/proton_p_vals*.csv > proton_p_vals.csv
+
